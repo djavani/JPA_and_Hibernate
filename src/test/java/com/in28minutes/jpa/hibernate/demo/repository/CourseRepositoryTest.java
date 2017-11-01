a2df8d88b8fa7518ef208c0a00386080009e8e3f
Djavani <djavanirgomes@gmail.com>
2017-11-01 16:47:15 -0200

    Criacao de teste unitario para metodo deleteById da classe CourseRepository

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.in28minutes.jpa.hibernate.demo.repository;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
@@ -26,6 +28,15 @@ public class CourseRepositoryTest {
 	public void findById_basic () {
 		Course course = repository.findById(10001L);
 		assertEquals("JPA in 50 Steps", course.getName());	
+		
+	}
+	
+	@Test
+	@DirtiesContext
+	public void deleteById_basic () {
+		repository.deletedById(10002L);
+		assertNull(repository.findById(10002L));
+			
 		
 	}
 
