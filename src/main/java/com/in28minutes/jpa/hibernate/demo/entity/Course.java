8c77989637714c19fda38bc85e56b972ff17d5f4
Djavani <djavanirgomes@gmail.com>
2017-11-01 14:31:15 -0200

    Commit inicial

---
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -0,0 +1,40 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String name;
+	
+	protected Course() {
+		
+	}
+
+	public Course(String name) {		
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+	
+	
+
+}
