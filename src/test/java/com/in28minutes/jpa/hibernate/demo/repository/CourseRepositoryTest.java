8a00034cc88e62aead991ee1ab4c0f0f960ce412
Djavani <djavanirgomes@gmail.com>
2017-11-04 16:30:01 -0200

    OneToOne Mapping - Retrieving Student with Passport and Eager Fetch

--- src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseRepositoryTest.java
@@ -3,6 +3,8 @@ package com.in28minutes.jpa.hibernate.demo.repository;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import javax.persistence.EntityManager;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Student;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=DemoApplication.class)
@@ -22,41 +25,17 @@ public class CourseRepositoryTest {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	CourseRepository repository;
+	StudentRepository repository;
+	
+	@Autowired
+	EntityManager em;
 	
 	@Test
-	public void findById_basic () {
-		Course course = repository.findById(10001L);
-		assertEquals("JPA in 50 Steps", course.getName());	
-		
+	public void retrieveStudentAndPassportDetails () {
+		Student student = em.find(Student.class, 20001L);
+		logger.info("Student -> {}", student);
+		logger.info("Passport details -> {}", student.getPassport());
 	}
 	
-	@Test
-	@DirtiesContext
-	public void deleteById_basic () {
-		repository.deletedById(10002L);
-		assertNull(repository.findById(10002L));
-	}
 	
-	@Test
-	@DirtiesContext
-	public void save_basic() {
-		
-		Course course = repository.findById(10001L);
-		assertEquals("JPA in 50 Steps", course.getName());
-		
-		course.setName("JPA in 50 Steps - UPDATED");	
-				
-		repository.save(course);
-		
-		Course course1 = repository.findById(10001L);
-		assertEquals("JPA in 50 Steps - UPDATED", course1.getName());
-	}
-	
-	@Test
-	@DirtiesContext
-	public void playWithEntityManager() {
-		repository.playWithEntityManager();
-	}
-
 }
---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.in28minutes.jpa.hibernate.demo.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.in28minutes.jpa.hibernate.demo.DemoApplication;
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=DemoApplication.class)
+public class StudentRepositoryTest {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	CourseRepository repository;
+	
+	@Test
+	public void findById_basic () {
+		Course course = repository.findById(10001L);
+		assertEquals("JPA in 50 Steps", course.getName());	
+		
+	}
+	
+	@Test
+	@DirtiesContext
+	public void deleteById_basic () {
+		repository.deletedById(10002L);
+		assertNull(repository.findById(10002L));
+	}
+	
+	@Test
+	@DirtiesContext
+	public void save_basic() {
+		
+		Course course = repository.findById(10001L);
+		assertEquals("JPA in 50 Steps", course.getName());
+		
+		course.setName("JPA in 50 Steps - UPDATED");	
+				
+		repository.save(course);
+		
+		Course course1 = repository.findById(10001L);
+		assertEquals("JPA in 50 Steps - UPDATED", course1.getName());
+	}
+	
+	@Test
+	@DirtiesContext
+	public void playWithEntityManager() {
+		repository.playWithEntityManager();
+	}
+
+}
