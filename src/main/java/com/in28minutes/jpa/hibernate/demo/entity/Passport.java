92765e4509959d0127e711330be4d1d513d48e0b
Djavani <djavanirgomes@gmail.com>
2017-11-04 21:18:56 -0200

    OneToOne Mapping - Bidirectional Relationship

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Passport.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Passport.java
@@ -2,8 +2,10 @@ package com.in28minutes.jpa.hibernate.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Passport {
@@ -15,6 +17,10 @@ public class Passport {
 	@Column(nullable = false)
 	private String number;	
 	
+	@OneToOne(fetch=FetchType.LAZY, mappedBy="passport")
+	private Student student;	
+	
+
 	protected Passport() {
 		
 	}
@@ -29,6 +35,14 @@ public class Passport {
 
 	public void setNumber(String number) {
 		this.number = number;
+	}
+	
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
 	}
 
 	public Long getId() {
